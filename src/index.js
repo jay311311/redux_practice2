09eed6aa1a81bdea2646b84fd6f59ac4a7dd1be0
jay311311 <jay311311@gmail.com>
2020-11-25 21:44:37 +0900

    list 제거

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ const ADD_TODO = "addTodo"
 const DELETE_TODO = "deleteTodo"
 
 
+
+const addtodo=text=>{
+  stateStore.dispatch({type:ADD_TODO, text})
+}
+
+const deleteTodo = id =>{
+  stateStore.dispatch({type:DELETE_TODO, id})
+}
+
+
+// mutations을 하지 않기 때문데 삭제를 위한 slice를 사용할수없다,
+// 그대신 filter 를 사용해 새로운 array를 생성할 것
 const paintTodo = () =>{
   const lists = stateStore.getState() 
   ul.innerHTML=""
@@ -16,17 +28,17 @@ const paintTodo = () =>{
     const btn = document.createElement("button")
     btn.innerText="del"
     li.innerText = element.text
-    li.id = element.text
+    li.id = element.id
+    btn.addEventListener("click",dispatchDeleteTodo)
     li.appendChild(btn)
     ul.appendChild(li)
   });
-
 }
 
 const reducer = (state = [], action ) => {
   switch(action.type){
-    case ADD_TODO : return [{text:action.text, id: Date.now()} , ...state ]
-    case DELETE_TODO: return []
+    case ADD_TODO : return [{id: Date.now(),text:action.text} , ...state ]
+    case DELETE_TODO: return  state.filter(todo=> todo.id !== action.id ) 
     default :return state
   } 
 }
@@ -38,22 +50,29 @@ const reducer = (state = [], action ) => {
   friends.push("kim")
   이것이 바로 mutation(변형)
   */
+ 
 
 const stateStore = createStore(reducer)
 
 stateStore.subscribe(paintTodo)
+stateStore.subscribe(()=>{console.log(stateStore.getState())})
 
 
+const dispatchAddTodo = text => {
+  addtodo(text)
+}
 
-const addTodo = text => {
-  stateStore.dispatch({type:ADD_TODO, text})
+const dispatchDeleteTodo =(event)=>{
+  const id = parseInt(event.target.parentNode.id)
+  console.log(id)
+  deleteTodo(id)
 }
 
 const handleSubmit = (event) => {
   event.preventDefault()
   const todo = input.value
   input.value=""
-   addTodo(todo)
+  dispatchAddTodo(todo)
 }
 
 form.addEventListener("submit", handleSubmit)
